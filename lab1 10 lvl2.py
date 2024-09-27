606184cacba5e2a960665c6f4d0ddd5fce588ef3
xyliganligma <katowice2014katowice@gmail.com>
2024-09-27 17:21:36 +0300

    Add files via upload

---
+++ lab1 10 lvl2.py
@@ -0,0 +1,7 @@
+L=float(0.1)
+Da=float(10**(-10))
+n=0
+while L>Da:
+    L=L/2
+    n+=1
+print(n)
---
+++ lab1 5 lvl1.py
@@ -0,0 +1,10 @@
+s=float(0)
+p=float(3)
+h=float(7)
+for i in range(10):
+    s=s+(p+i*h)**2
+print(s)
+
+'''p=float(3)
+h=float(7)
+print(p**2 + (p+h)**2 + (p+2*h)**2 + (p+3*h)**2 + (p+4*h)**2 + (p+5*h)**2 + (p+6*h)**2 + (p+7*h)**2 + (p+8*h)**2 + (p+9*h)**2 )'''
